2b31373c03bbb053d992035a81fe6209e08f86e0
EthanLewis <tuk32865@temple.edu>
2021-04-14 20:09:26 -0400

    Basic function outline for Sig gen,reporter, catcher, and intervals

---
+++ cmake-build-debug/CMakeFiles/Signals.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Signals.dir/main.c.obj"
+  "Signals.exe"
+  "Signals.exe.manifest"
+  "Signals.pdb"
+  "libSignals.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Signals.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
